45f49f4181264286dd378917df50baeef6c9a343
iballi2017 <iballi2017@gmail.com>
2020-08-29 19:26:43 +0100

    user Order details page restructuring

--- includes/header.php
+++ includes/header.php
@@ -27,7 +27,7 @@
             <button class="navbar-toggler mobile-menu-icon" type="button" data-toggle="collapse"
                 data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
-                <i class="fas fa-ellipsis-v fa-1x"></i>
+                <i class="fas fa-bars fa-1x"></i>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
