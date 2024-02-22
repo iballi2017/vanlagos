2550a485e690a12127550f899f344c4e9930a6bd
iballidev <iballi.dev@gmail.com>
2024-02-22 10:09:25 +0100

    correct the loader icon on page load

--- js/app.jquery.js
+++ js/app.jquery.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
     /* page loader */
     var loading = $("#loading");
-    $(window).on('load', function() {
         if (loading) {
             loading.hide();
         }
-    });
 })
